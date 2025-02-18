29b41b053c2dc5bb04b223705e001b69ce0674fa
Ana Gabriela <97294208+aagablm@users.noreply.github.com>
2025-02-18 20:47:37 -0300

    adicionando novo teste

---
+++ cypress/e2e/acessarPaginaInicial.cy.js
@@ -0,0 +1,12 @@
+describe("Página Inicial", () => {
+    it("Deve carregar a página inicial", () => {
+      cy.visit("http://localhost:3000");
+  
+      // Verifica se o título da página está correto
+      cy.contains("Minha Loja");
+  
+      // Verifica se o botão 'Adicionar ao Carrinho' está presente
+      cy.get("button").contains("Adicionar ao Carrinho")
+
+  });
+  
